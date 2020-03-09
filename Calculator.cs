b0a880d18dc2cce15477942b04c2cb2803a224d4
Chroizen <aryobimo1231@gmail.com>
2020-03-09 16:59:38 +0700

    Calculator tugas 2 sampun perfect

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,20 @@
     {
         static void Main(string[] args)
         {
+		Console.Title = "Aplikasi ngitung";
 
+            Console.Write("inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("inputkan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+			
+			Console.WriteLine("\ntekan tombol ash mbuh");
+            Console.ReadKey();
         }
         static int Penambahan(int a, int b)
         {
