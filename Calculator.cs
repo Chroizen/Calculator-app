ba5aa888578a1a6a0906bca1d311026bdc244359
Chroizen <aryobimo1231@gmail.com>
2020-03-09 16:33:38 +0700

    project Calculator

---
+++ Calculator.cs
@@ -0,0 +1,13 @@
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
